8313e0075f3e26a22dca30488fd76a0eb146108e
spasitenas <mirea5sem@gmail.com>
2024-09-23 14:37:46 +0300

    Create 1.js

---
+++ prak3/1.js
@@ -0,0 +1,28 @@
+// Задайте здесь имя датчика температуры
+var temperatureSensor = "wb-w1/28-00000abcdef/temperature";
+
+// Задайте здесь имя реле вентилятора
+var fanRelay = "wb-gpio/Relay_1";
+
+// Установите пороговые значения температуры
+var temperatureOn = 30; // Температура, выше которой вентилятор включается
+var temperatureOff = 25; // Температура, ниже которой вентилятор выключается
+
+// Функция, которая выполняется при изменении температуры
+defineRule("FanControlByTemperature", {
+  whenChanged: temperatureSensor,
+  then: function (newValue, devName, cellName) {
+    log("Текущая температура: " + newValue + "°C");
+
+    if (newValue >= temperatureOn) {
+      // Включаем вентилятор
+      dev[fanRelay] = 1;
+      log("Вентилятор включен");
+    }
+    else if (newValue <= temperatureOff) {
+      // Выключаем вентилятор
+      dev[fanRelay] = 0;
+      log("Вентилятор выключен");
+    }
+  }
+});
